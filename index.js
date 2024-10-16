1887d66968c1a0033bc2a53f2a10c146d51ecd21
liamrl <liamrenaudlevine@gmail.com>
2024-10-16 15:19:48 -0400

    upd progress 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.getElementById("count-el").innerText = 5
+let count = 0;
+function increment(){
+    count = count + 1
+    document.getElementById("count-el").innerText = count
+}
+document.body.createElement()
+function save(){
+    
+}
+
