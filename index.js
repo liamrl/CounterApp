4dc2916aff1622431e69edffebef48e3f9c47fe1
liamrl <liamrenaudlevine@gmail.com>
2024-10-16 15:34:42 -0400

    end of day 10/16

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ function increment(){
     count = count + 1
     document.getElementById("count-el").innerText = count
 }
-document.body.createElement()
 function save(){
-    
+    let save1 = 0;
+    save1 = count
+    document.getElementById("save2").innerText = save2.innerText + save1 + "-"
 }
 
